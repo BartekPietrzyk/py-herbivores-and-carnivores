8306d7a5c6434e933b26cded122315dc4681c970
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-04-27 13:48:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100,
-                 hidden: bool = False) -> None:
+    def __init__(self, name: str,
+                 health: int = 100, hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
-        if self.health > 0:
+        if self.health > 0 and self not in Animal.alive:
             Animal.alive.append(self)
 
     def animal_alive(self) -> bool:
@@ -19,16 +19,18 @@ class Animal:
             return True
         return False
 
-    def __str__(self) -> str:
+    def take_damage(self, damage: int) -> None:
+        self.health -= damage
+        if self.health <= 0:
+            self.animal_dead()
 
-        return (
-            f"{{Name: {self.name},"
-            f"" f" Health: {self.health}, Hidden: {self.hidden}}}"
-        )
+    def __str__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
     @classmethod
     def print_alive(cls) -> str:
-
         return "[" + ", ".join(str(animal) for animal in cls.alive) + "]"
 
 
@@ -40,9 +42,4 @@ class Herbivore(Animal):
 class Carnivore(Animal):
     def bite(self, herbivore: Herbivore) -> None:
         if isinstance(herbivore, Herbivore) and not herbivore.hidden:
-            herbivore.health -= 50
-            if herbivore.health <= 0:
-                herbivore.animal_dead()
-
-
-print(Animal.print_alive())
+            herbivore.take_damage(50)
