fa343cd12799d76e5209aa75284b088335133b66
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-04-27 13:58:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 class Animal:
     alive = []
-    def __init__(self, name: str, health=100, hidden=False) -> None:
+
+    def __init__(self, name: str, health: str = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         if self.health > 0:
             Animal.alive.append(self)
-
 
     def animal_alive(self) -> bool:
         return self.health > 0
@@ -18,15 +19,17 @@ class Animal:
         return False
 
     def __str__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
-
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
     @classmethod
-    def print_alive(cls):
+    def print_alive(cls) -> str:
         return "[" + ", ".join(str(animal) for animal in cls.alive) + "]"
 
+
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
